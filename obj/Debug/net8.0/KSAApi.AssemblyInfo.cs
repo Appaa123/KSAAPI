67b6e3781674fb0286e624045cd1d9ffc9dbd364
appaarao22@gmail.com <appaarao22@gmail.com>
2025-05-04 23:03:44 +0530

    Updated MongoDB.Driver.Core

--- obj/Debug/net8.0/KSAApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/KSAApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf1ca18f98659d9c4ea18566af228b77ca933dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442f1b500316298b2b34bf1cabce601a4b759e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
