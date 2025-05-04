daf1ca18f98659d9c4ea18566af228b77ca933dd
appaarao22@gmail.com <appaarao22@gmail.com>
2025-05-04 22:31:42 +0530

    Updated MongoDB version

--- obj/Debug/net8.0/KSAApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/KSAApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788f75602407628aac0524a7c03fbaeed4620f96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a98018e19f99672a9e5d976405dff4ad8ed0fdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
