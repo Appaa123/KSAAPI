5a98018e19f99672a9e5d976405dff4ad8ed0fdb
appaarao22@gmail.com <appaarao22@gmail.com>
2025-05-04 22:21:25 +0530

    Latest changes

--- obj/Release/net8.0/KSAApi.AssemblyInfo.cs
+++ obj/Release/net8.0/KSAApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb0dc95fe9ff60a101545502b4e5ee7679bec72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788f75602407628aac0524a7c03fbaeed4620f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KSAApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
